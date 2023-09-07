6d06d3838a5c5280102b588d35253bfc0ac351c2
Vinitjp123 <vinitjpanchal2002@gmail.com>
2023-09-07 18:19:40 +0530

    implemented some models

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 
+const CityRepository = require('./repository/cityRepository')
 const {PORT} = require('./config/serverConfig')
 
 const setupandStartServer = async ()=>{
