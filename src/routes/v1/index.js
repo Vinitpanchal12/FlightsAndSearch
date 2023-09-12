2ea162e2739c25e949e5dcbc1be0716d1c67b565
Vinitjp123 <vinitjpanchal2002@gmail.com>
2023-09-12 19:55:50 +0530

    minor bugs

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const CityController = require('../../controllers/cityController')
 
 router.post('/city', CityController.create);
+router.get('/city/:id', CityController.get);
+router.delete('/city/:id',CityController.destroy);
+router.patch('/city/:id', CityController.update);
 
 module.exports = router;
 
